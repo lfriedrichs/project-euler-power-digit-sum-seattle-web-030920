3a0c705dfa2e585b31a0074d24fd2ba143c38add
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 15:51:56 +0000

    Automatically backed up by Learn

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,10 +1,5 @@
 # code your solution here
 def power_digit_sum(base, power)
   number = base ** power 
-  digits = []
-  number.size.times do 
-    digits << number % 10 
-    number = number / 10 
-  end
-  digits.sum
+  number.digits.sum 
 end
